27d93c4e6bc48926b67e49dc1ce55583d1afb8e3
maki-sig <mnbotis.nhcs@gmail.com>
2026-03-08 22:24:26 +0800

    Test 7

--- CSharpParser.java
+++ CSharpParser.java
@@ -3,7 +3,7 @@ class CSharParser {
         System.out.println("\nHello Kinaban");
         print("New way to print");
         print("What is my name");
-        print("My name is John Doe\n");
+        print("My name is John Doe\nI changed a line");
     }
     private static void print(String text){
         System.out.println(text);
